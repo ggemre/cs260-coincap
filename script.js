96cf421b109f14e56350b5c2c5a0b7f971a4b3ef
Gage Moore <gmoore1@byu.edu>
2022-09-19 02:37:54 +0000

    added style

--- script.js
+++ script.js
@@ -9,11 +9,18 @@ document.getElementById('coinSubmit').addEventListener('click', function(event) 
         .then(function(response) {
             return response.json();
         }).then(function(json) {
-            const priceStr = `The cost of one ${json.data.name} is $${Number(json.data.priceUsd).toFixed(2)} USD.`;
+            const priceStr = `The cost of ${json.data.name} is $${Number(json.data.priceUsd).toFixed(2)} USD.`;
             const changeStr = Number(json.data.changePercent24Hr).toFixed(2);
             const infoStr = `${json.data.name}, (${json.data.symbol}), has a supply of ${Number(json.data.supply).toFixed(2)} and a market cap of $${Number(json.data.marketCapUsd).toFixed(2)} USD `;
             document.getElementById('price').textContent = priceStr;
             document.getElementById('change').textContent = changeStr;
             document.getElementById('info').textContent = infoStr;
+            
+            if (changeStr < 0) {
+                document.getElementById('change').style.color = 'red';
+            } else {
+                document.getElementById('change').style.color = 'green';
+            }
         });
+        document.getElementById('change').style.display = 'block';
 });
